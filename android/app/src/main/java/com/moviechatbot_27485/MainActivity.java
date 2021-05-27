8a2020b46928caeb1e76c16414114d08a82c9a0a
Crowdbotics <team@crowdbotics.com>
2021-05-27 19:07:43 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/moviechatbot_27485/MainActivity.java
@@ -0,0 +1,15 @@
+package com.moviechatbot_27485;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "moviechatbot_27485";
+  }
+}
